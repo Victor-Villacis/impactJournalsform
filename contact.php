053dc16d923fee66d82413a9df5ebeb7714b5433
vicrtor villacis <victor.villacis@outlook.com>
2016-05-25 21:43:16 -0400

    added map features

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+
+// configure variables
+$from = 'info@oncotarget.com';
+$sendTo = 'support@oncotarget.com';
+$subject = 'New message from contact form';
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'address'=> 'Address', 'subject' => 'Subject' 'message' => 'Message'); // array variable name => Text to appear in email
+$okMessage = 'Contact form successfully submitted. Thank you!';
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
+// sending out the form
+
+try
+{
+    $emailText = "You have new message from contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    mail($sendTo, $subject, $emailText, "From: " . $from);
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    
+    header('Content-Type: application/json');
+    
+    echo $encoded;
+}
+else {
+    echo $responseArray['message'];
+}
--- js/form.js
+++ js/form.js
@@ -1,9 +1,25 @@
 //alert("hello")
 
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
+// var map;
+//       function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: 42.768359, lng: },
+//           zoom: 15
+//         });
+//       }
+
+
+function initMap() {
+  var myLatLng = {lat: 42.768359, lng: -78.739719};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Impact Journals'
+  });
+}
