97813330841ebc67bf46ee1339f750a5b1e02b1a
vicrtor villacis <victor.villacis@outlook.com>
2016-05-25 20:36:49 -0400

    built out body

--- js/form.js
+++ js/form.js
@@ -1,1 +1,9 @@
 //alert("hello")
+
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
