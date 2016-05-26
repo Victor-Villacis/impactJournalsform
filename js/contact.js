75ca67b052f9b2032ca7f8206d7970b377aea229
vicrtor villacis <victor.villacis@outlook.com>
2016-05-26 04:54:50 -0400

    changed whole style and look

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,9 @@
 $(function () {
-    alert("hi")
-
     $('#contact-form').validator();
-
     $('#contact-form').on('submit', function (e) {
         if (!e.isDefaultPrevented()) {
             var url = "contact.php";
-
+// ajax call
             $.ajax({
                 type: "POST",
                 url: url,
@@ -15,7 +12,6 @@ $(function () {
                 {
                     var messageAlert = 'alert-' + data.type;
                     var messageText = data.message;
-
                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
                     if (messageAlert && messageText) {
                         $('#contact-form').find('.messages').html(alertBox);
--- js/map.js
+++ js/map.js
@@ -1,9 +1,18 @@
-function initMap() {
-  var myLatLng = {lat: 42.768359, lng: -78.739719};
 
+  function initMap() {
+  var myLatLng = {lat: 42.768359, lng: -78.739719};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: myLatLng
+    zoom: 14,
+    center: myLatLng,
+    scrollwheel: false,
+    draggable: false,
+    disableDefaultUI: true,
+  });
+
+  var contentString = 'Impact Journals';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
   });
 
   var marker = new google.maps.Marker({
@@ -11,4 +20,10 @@ function initMap() {
     map: map,
     title: 'Impact Journals'
   });
+
+function run() {
+  return  infowindow.open(map, marker);
+} run()
+
 }
+
