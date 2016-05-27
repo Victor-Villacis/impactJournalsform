45e7792ef8f35fc57dedc8ee9bd9dbaff3a28e41
vicrtor villacis <victor.villacis@outlook.com>
2016-05-27 11:27:16 -0400

    adjusted css and col-

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
   function initMap() {
   var myLatLng = {lat: 42.768359, lng: -78.739719};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
+    zoom: 15,
     center: myLatLng,
     scrollwheel: false,
     draggable: false,
